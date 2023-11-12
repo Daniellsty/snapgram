2e01eaa7a24bfdcd56e11c9182a7af3987df18e9
danyal <danyallsty04@gmail.com>
2023-11-12 05:26:35 -0800

    user auth logic

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,5 +1,5 @@
 import { INewUser } from "@/types";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, databases } from "./config";
 import { ID } from "appwrite";
 
 export async function createUserAccount(user:INewUser){
@@ -14,12 +14,54 @@ export async function createUserAccount(user:INewUser){
 
         )
 
-        return newAccount;
+        if(!newAccount) throw Error;
+        const avatarUrl = avatars.getInitials(user.name);
+
+        const newUser = await saveUserToDB({
+            accountId:newAccount.$id,
+            email:newAccount.email,
+            name:newAccount.name,
+            imageUrl:avatarUrl,
+            username:user.username,
+        })
         
+        return newUser;
     } catch (error) {
         console.log(error);
         return error
         
     }
+
+}
+
+
+export async function saveUserToDB(user : 
+    
+    {
+        accountId :string,
+        email: string,
+        name : string,
+        imageUrl :URL,
+        username?:string,
+    }
+
+    ){
+
+        try {
+
+            const newUser = await databases.createDocument(
+                appwriteConfig.databaseId,
+                appwriteConfig.userCollectionId,
+                ID.unique(),
+                user,
+            )
+
+            return newUser;
+            
+        } catch (error) {
+            console.log(error);
+            return error
+            
+        }
 
 }
