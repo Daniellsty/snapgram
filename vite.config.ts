e7673414c6e4457124abeba5b0206e3a4ee42150
danyal <danyallsty04@gmail.com>
2023-11-12 03:39:53 -0800

    icons and images added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig({
+  server:{
+    host:"localhost",
+    port:3000,
+  
+  },
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
