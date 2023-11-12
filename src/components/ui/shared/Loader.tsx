b2a799154a6cb6ea20ae2f9069195d33588c1645
danyal <danyallsty04@gmail.com>
2023-11-12 05:03:26 -0800

    loader component

---
+++ src/components/ui/shared/Loader.tsx
@@ -0,0 +1,15 @@
+import loader from '../../../assets/icons/loader.svg'
+const Loader = () => {
+  return (
+    <div className='flex-center w-full'>
+
+        <img src={loader} alt="loader"
+        width={24}
+        height={24}
+        
+        />
+    </div>
+  )
+}
+
+export default Loader
