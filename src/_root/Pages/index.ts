9e4dcd36714cae22aeda97fddb03921b9a270489
danyal <danyallsty04@gmail.com>
2023-11-17 01:22:23 -0800

    abbreviation exporting routes to app

--- src/_root/Pages/index.ts
+++ src/_root/Pages/index.ts
@@ -1,2 +1,11 @@
 export {default as Home} from './Home.tsx'
 export {default as RootLayout} from './RootLayout.tsx'
+export { default as Explore } from "./Explore";
+export { default as Saved } from "./Saved";
+export { default as CreatePost } from "./CreatePost";
+export { default as Profile } from "./Profile";
+export { default as UpdateProfile } from "./UpdateProfile";
+export { default as EditPost } from "./EditPost";
+export { default as PostDetail } from "./PostDetail";
+export { default as LikedPosts } from "./LikedPosts";
+export { default as AllUsers } from "./AllUsers";
